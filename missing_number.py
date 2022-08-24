7e2246067bd3c50a5cb1fda272d4bdc36acba55f
Astenik <106985461+Astenik@users.noreply.github.com>
2022-08-24 14:14:17 +0400

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -2,12 +2,8 @@ def missing_number(nums:list):
         '''given an array nums (as parametre) containing n distinct numbers
         in the range [0, n], this function returns the only number in the 
         range that is missing from the array.'''
-        n = len(nums)
-        for number in range(n): # iterates over number [0, n]
-             for num in nums: # iterates over list
-                  if number == num: # checs if number is in list or not
-                       break
-             else:
-                  return number 
+        for i in range(len(nums)):
+          if nums[i] != i:
+              return i 
 nums = [1, 0, 3, 2, 8, 6, 7, 4, 9]
 print(f"the missing number is: {missing_number(nums)}")
